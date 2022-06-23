962502cf19deae36c4d0ba9baff182f410a5c075
kawshikbuet17 <kawshik.kumar.paul@gmail.com>
2022-06-24 03:33:58 +0600

    nodejs project - Create Data (file system)

--- Raw-NodeJS-API-Project/index.js
+++ Raw-NodeJS-API-Project/index.js
@@ -2,9 +2,17 @@
 const http = require('http');
 const {handleReqRes} = require('./helpers/handleReqRes');
 const environment = require('./helpers/environments');
+const data = require('./lib/data');
 
 //app object - module scaffolding
 const app = {};
+
+//testing file system
+//manually create .data/test folder
+//our code can create file, not folder till now
+data.create('test', 'newFile', {'name':'KKP', 'dept':'BUET CSE'}, (err)=>{
+    console.log('error is', err);
+});
 
 //create server
 app.createServer = ()=>{
---
+++ Raw-NodeJS-API-Project/lib/data.js
@@ -0,0 +1,36 @@
+//dependencies
+const fs = require('fs');
+const path = require('path');
+const lib = {};
+
+//base dir of the data folder
+lib.basedir = path.join(__dirname, '../.data/');
+
+//write data to file
+lib.create = function(dir, file, data, callback){
+    fs.open(lib.basedir+dir+'/'+file+'.json', 'wx', (err, fileDescriptor)=>{
+        if(!err && fileDescriptor){
+            //convert data to string
+            const stringData = JSON.stringify(data);
+
+            //write data to file
+            fs.writeFile(fileDescriptor, stringData, (err2)=>{
+                if(!err2){
+                    fs.close(fileDescriptor, (err3)=>{
+                        if(!err3){
+                            callback(false);
+                        }else{
+                            callback('Error closing the new file');
+                        }
+                    });
+                }else{
+                    callback('Error writing to new file');
+                }
+            });
+        }else{
+            callback('Could not create new file. It may already exists');
+        }
+    });
+};
+
+module.exports = lib;
