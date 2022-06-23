5e5e9cd820f98aed939bf826b0a72d7ca4528e86
kawshikbuet17 <kawshik.kumar.paul@gmail.com>
2022-06-23 14:48:45 +0600

    nodejs project - Refactoring Code

---
+++ Raw-NodeJS-API-Project/helpers/handleReqRes.js
@@ -0,0 +1,68 @@
+//dependencies
+const url = require('url');
+const {StringDecoder} = require('string_decoder');
+
+//module scaffolding
+const handler = {};
+
+
+//handle Request Response
+handler.handleReqRes = (req, res)=>{
+    //request handle
+
+    //get the url and parse it
+    const parsedUrl = url.parse(req.url, true); //true for considering query string. such as ../?a=5&b=7
+    //false will ignore the query string
+    
+    //browse this http://localhost:3000/about?a=5&b=7
+    //and notice the console
+    // console.log(parsedUrl);
+
+    const path = parsedUrl.pathname;
+    //console.log(path); //print pathname
+
+    //concern: /about, about/, /about/ are same
+    //so we must ignore the unwanted slash
+    //to do this, we have to use regular expression
+    //this regex will ignore the starting slash and ending slash
+    //won't ignore then slash of middle of something
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    // console.log(trimmedPath);
+
+    //to know GET method or POST method
+    const method = req.method.toLowerCase();
+
+    //to get query string
+    //such as ../?a=5&b=7
+    //it is got as an object
+    const queryStringObject = parsedUrl.query;
+    // console.log(queryStringObject);
+
+    //headers means metadata
+    //which comes with the requests
+    //we can also send custom additional meta data with the request
+    const headersObject = req.headers;
+    console.log(headersObject);
+
+    //GET request data is sent as query string
+    //but in POST request data is sent with request body
+    //if in frontend, a form is filled up, the form data is sent as request body
+    //this is called request payload also
+    //this data comes as stream and buffer
+    //we have to decode it
+    //to test this, provide data from postman (as body and POST request)
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+    req.on('data', (buffer)=>{
+        realData += decoder.write(buffer);
+    });
+    req.on('end', ()=>{
+        realData += decoder.end();
+        console.log(realData);
+
+        //response handle
+        res.end('Hello World');
+    });
+}
+
+module.exports = handler;
